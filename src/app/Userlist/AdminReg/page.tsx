e706df99cde71d59bdb381caf66c82ab71113a55
INFINITECH-OJT2025 <infinitechojt2025@gmail.com>
2025-03-28 09:16:18 +0800

    Update page.tsx

--- src/app/Userlist/AdminReg/page.tsx
+++ src/app/Userlist/AdminReg/page.tsx
@@ -153,6 +153,10 @@ const Dashboard = () => {
       <div className="flex min-h-screen bg-gray-900 text-white">
         <Adminbar />
 
+             <div className="sticky top-0 h-screen w-64 bg-gray-800 shadow-lg hidden md:block">
+      
+      </div>
+
         <div className="flex-1 p-6">
         <div className="flex justify-center items-center ">
   <h3 className="text-3xl font-bold text-green-500">Admin Dashboard</h3>
