5fccee09cb779770eac3f6d0ddb5b4a080040c10
INFINITECH-OJT2025 <infinitechojt2025@gmail.com>
2025-03-28 16:51:16 +0800

    Update page.tsx

--- src/app/Taskuser/page.tsx
+++ src/app/Taskuser/page.tsx
@@ -27,7 +27,7 @@ interface Activity {
   collaborator_name?: string,
 }
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "https://infinitech-api5.site/api";
 
 const ProgressBar = ({ percentage }: { percentage: number }) => {
   return (
