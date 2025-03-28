4b9ffd79984c9fea2381fad1780a70598f0a3f04
INFINITECH-OJT2025 <infinitechojt2025@gmail.com>
2025-03-28 10:57:06 +0800

    Update page.tsx

--- src/app/UserProfile/page.tsx
+++ src/app/UserProfile/page.tsx
@@ -98,9 +98,7 @@ const TodoPage = () => {
       <div className={`flex-1 flex items-center justify-center p-4 md:p-6 transition-transform duration-300 ${editing ? 'translate-x-[-300px]' : ''}`}>
   
         <div className="card w-full max-w-4xl h-auto bg-gray-900 rounded-xl shadow-2xl p-6 flex flex-col md:flex-row border-4 border-green-900 relative">
-          <h1 className="absolute top-[-96px] left-1/2 transform -translate-x-1/2 text-3xl md:text-5xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 drop-shadow-lg">
-            USER PROFILE
-          </h1>
+       
 
           <div className="img-container w-full md:w-2/4 h-auto p-4 bg-gray-900 rounded-xl overflow-hidden flex items-center justify-center border-4 border-gray-700 mt-8">
             {user?.profile_image ? (
